006427bc9d8164f8c8af1e80dfd4aa6c095ad645
li-chuang <278747148@qq.com>
2016-12-27 23:31:30 +0800

    Create ScannerTest.java

---
+++ com/lichuang/chap01/ScannerTest.java
@@ -0,0 +1,17 @@
+package com.lichuang.chap01;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		
+		System.out.println(name+" , 你好~");
+		
+		
+		 
+	}
+
+}
+a
