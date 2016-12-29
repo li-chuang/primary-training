0f4d4b373bd3014fe160be2edceea992d596e7b8
li-chuang <278747148@qq.com>
2016-12-29 23:35:59 +0800

    Update MulityTest.java

--- com/lichuang/chap01/MulityTest.java
+++ com/lichuang/chap01/MulityTest.java
@@ -1,5 +1,7 @@
 package com.lichuang.chap01;
 
+import java.text.DecimalFormat;
+
 /**
  * 打印乘法表
  * @author ASUS
@@ -7,13 +9,14 @@ package com.lichuang.chap01;
  */
 public class MulityTest {
 	public static void main(String[] args) {
-		mulity(9);
+		mulity(12);
 	}
 	
 	private static void mulity(int n){
+		DecimalFormat format = new DecimalFormat("0000");
 		for(int i = 1;i<=n;i++){
 			for(int j = 1;j<=i;j++){
-				System.out.print(j+" * "+i +" = " +i*j+"\t");
+				System.out.print(format.format(j)+" * "+format.format(i) +" = " +format.format(i*j)+"  ");
 			}
 			System.out.println("");
 		}
