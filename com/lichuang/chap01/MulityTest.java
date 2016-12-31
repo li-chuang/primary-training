554cd3d2d36edf8e5e6095e3453164d2882022e5
li-chuang <278747148@qq.com>
2016-12-31 22:06:58 +0800

    Update MulityTest.java

--- com/lichuang/chap01/MulityTest.java
+++ com/lichuang/chap01/MulityTest.java
@@ -13,12 +13,16 @@ public class MulityTest {
 	}
 	
 	private static void mulity(int n){
-		DecimalFormat format = new DecimalFormat("0000");
+		//DecimalFormat format = new DecimalFormat("0000");
 		for(int i = 1;i<=n;i++){
 			for(int j = 1;j<=i;j++){
-				System.out.print(format.format(j)+" * "+format.format(i) +" = " +format.format(i*j)+"  ");
+				//System.out.print(format.format(j)+" * "+format.format(i) +" = " +format.format(i*j)+"  ");
+				//String.format("%04d", youNumber)
+				System.out.print(String.format("%3d", j)+" *"+String.format("%3d", i) +" =" +String.format("%3d", i*j)+"  ");
+				
 			}
 			System.out.println("");
 		}
 	}
+
 }
