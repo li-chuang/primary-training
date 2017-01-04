dabaab823c475220ab828823eeeef131ea59fd60
li-chuang <278747148@qq.com>
2017-01-04 23:24:28 +0800

    Create ReversTest.java

---
+++ com/lichuang/chap02/ReversTest.java
@@ -0,0 +1,19 @@
+package com.lichuang.chap02;
+
+import java.util.List;
+
+/**
+ * 写函数逆转列表，最好是原地逆转
+ *
+ */
+public class ReversTest {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private static void reverse(List<String> list){
+		
+	}
+
+}
