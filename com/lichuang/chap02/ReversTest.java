2ea36600191d86096eaac8fa2feba102742518df
li-chuang <278747148@qq.com>
2017-01-05 00:02:55 +0800

    Update ReversTest.java

--- com/lichuang/chap02/ReversTest.java
+++ com/lichuang/chap02/ReversTest.java
@@ -1,5 +1,6 @@
 package com.lichuang.chap02;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,11 +10,25 @@ import java.util.List;
 public class ReversTest {
 	
 	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add("D");
+		list.add("E");
+		System.out.println(reverse(list));
 		
 	}
 	
-	private static void reverse(List<String> list){
-		
+	private static List<String> reverse(List<String> list){
+		String tmp = null;
+		int count = list.size();
+		for(int i=0;i<count/2;i++){
+			tmp = list.get(i);
+			list.set(i, list.get(count-i-1));
+			list.set(count-i-1, tmp);
+		}
+		return list;
 	}
 
 }
