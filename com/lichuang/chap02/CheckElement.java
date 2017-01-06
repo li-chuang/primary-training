5647bf481178b8ac5db04e471f2d33ca0ac6ce74
li-chuang <278747148@qq.com>
2017-01-06 23:00:25 +0800

    Create CheckElement.java

---
+++ com/lichuang/chap02/CheckElement.java
@@ -0,0 +1,31 @@
+package com.lichuang.chap02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 检查指定的元素是否出现在列表中
+ *
+ */
+public class CheckElement {
+
+	public static void main(String[] args) {
+		List<Object> list = new ArrayList<Object>();
+		list.add(1);
+		list.add("hello");
+		list.add("world");
+		list.add(2);
+		boolean flag =  checkElement(list,"hello");
+		System.out.println("是否存在："+flag);
+		
+	}
+	
+	private static boolean checkElement(List<Object> list,Object o){
+		for (Object object : list) {
+			if(object == o){
+				return true;
+			}
+		}		
+		return false;
+	}
+}
