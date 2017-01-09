c76be879dc9f972239f3251c063df7c3732b1630
li-chuang <278747148@qq.com>
2017-01-09 23:01:44 +0800

    Create MergeSortedList.java

---
+++ com/lichuang/chap02/MergeSortedList.java
@@ -0,0 +1,21 @@
+package com.lichuang.chap02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 合并两个有序的列表 
+ *
+ */
+public class MergeSortedList {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private static List<Object> mergeSortedList(List<Object> list1,List<Object> list2){
+		List<Object> list = new ArrayList<Object>();
+		
+		return list;
+	}
+}
